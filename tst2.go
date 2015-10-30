59a02fdc348a5396a0c2bb67771c414a1d502999
Sergey Klimkin <sklimkin@index.ru>
2015-10-30 17:57:12 +0300

    debug log removed

--- tst2.go
+++ tst2.go
@@ -6,7 +6,7 @@ package tst
 
 import "unicode/utf8"
 
-// import "github.com/ondi/go-log"
+const INTMAX = 1 << 32 - 1
 
 type TernaryNode2_t struct {
 	hi_kid int
@@ -29,35 +29,24 @@ func (self * TernaryTree2_t) Add(str string, value string) {
 		last = -1
 	}
 	for _, key = range str {
-		// log.Trace("enter loop key = '%c' cur = %v, last = %v, len = %v", key, cur, last, len(self.root))
 		for cur != -1 && key != self.root[cur].key {
 			last = cur
 			if key < self.root[cur].key {
-				// log.Trace("'%v' < '%v': going to [%v].lo_kid = %v", key, self.root[cur].key, cur, self.root[cur].lo_kid)
-				if self.root[cur].lo_kid == -1 {
-					self.root[cur].lo_kid = len(self.root)
-					// log.Trace("self.root[%v].lo_kid = %v", last, self.root[cur].lo_kid)
-					cur = -1
-				} else {
-					cur = self.root[cur].lo_kid
+				cur = self.root[cur].lo_kid
+				if cur == -1 {
+					self.root[last].lo_kid = len(self.root)
 				}
 			} else {
-				// log.Trace("'%v' > '%v': going to [%v].hi_kid = %v", key, self.root[cur].key, cur, self.root[cur].hi_kid)
-				if self.root[cur].hi_kid == -1 {
-					self.root[cur].hi_kid = len(self.root)
-					// log.Trace("self.root[%v].hi_kid = %v", last, self.root[cur].hi_kid)
-					cur = -1
-				} else {
-					cur = self.root[cur].hi_kid
+				cur = self.root[cur].hi_kid
+				if cur == -1 {
+					self.root[last].hi_kid = len(self.root)
 				}
 			}
 		}
 		if cur == -1 {
 			cur = len(self.root)
-			// log.Trace("creating new node cur = %v, last = %v", cur, last)
 			if last != -1 && self.root[last].eq_kid == -1 {
 				self.root[last].eq_kid = cur
-				// log.Trace("self.root[%v].eq_kid = %v", last, cur)
 			}
 			self.root = append(self.root, TernaryNode2_t{key: key, eq_kid: -1, hi_kid: -1, lo_kid: -1})
 		}
@@ -67,7 +56,6 @@ func (self * TernaryTree2_t) Add(str string, value string) {
 	if last != -1 {
 		self.root[last].value = value
 	}
-	// log.Trace("%v", self.root)
 }
 
 func (self * TernaryTree2_t) Search(str string) (int, int, string, bool) {
@@ -80,7 +68,6 @@ func (self * TernaryTree2_t) Search(str string) (int, int, string, bool) {
 		cur = -1
 	}
 	for n, key = range str {
-		// log.Trace("search '%c' cur = %v", key, cur)
 		for cur != -1 && key != self.root[cur].key {
 			if key < self.root[cur].key {
 				cur = self.root[cur].lo_kid
