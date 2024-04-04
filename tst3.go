4f75e49d2bb4843b1a88a268d231ca42d29f0698
ondi <thy321@gmail.com>
2024-04-05 00:49:40 +0300

    minor fix

--- tst3.go
+++ tst3.go
@@ -12,47 +12,48 @@ const (
 type key3_t struct {
 	i    int
 	hash uint64
+	code rune
+}
+
+type mapped_t[Value_t any] struct {
+	value Value_t
 }
 
 type Tree3_t[Value_t any] struct {
-	root map[key3_t]*Value_t
+	root map[key3_t]*mapped_t[Value_t]
 }
 
 func NewTree3[Value_t any]() *Tree3_t[Value_t] {
 	return &Tree3_t[Value_t]{
-		root: map[key3_t]*Value_t{},
+		root: map[key3_t]*mapped_t[Value_t]{},
 	}
 }
 
-func (self *Tree3_t[Value_t]) Add(in string, value Value_t) {
+func (self *Tree3_t[Value_t]) Add(prefix string, value Value_t) {
 	var ok bool
 	key := key3_t{hash: FnvOffset64}
-	for i, v := range in {
-		key.i = i
-		key.hash ^= uint64(v)
+	for key.i, key.code = range prefix {
+		key.hash ^= uint64(key.code)
 		key.hash *= FnvPrime64
 		if _, ok = self.root[key]; !ok {
 			self.root[key] = nil
 		}
 	}
-	temp := new(Value_t)
-	*temp = value
-	self.root[key] = temp
+	self.root[key] = &mapped_t[Value_t]{value: value}
 }
 
 func (self *Tree3_t[Value_t]) Search(in string) (value Value_t, ok bool) {
 	var count int
-	var temp *Value_t
+	var temp *mapped_t[Value_t]
 	key := key3_t{hash: FnvOffset64}
-	for i, v := range in {
-		key.i = i
-		key.hash ^= uint64(v)
+	for key.i, key.code = range in {
+		key.hash ^= uint64(key.code)
 		key.hash *= FnvPrime64
 		if temp, ok = self.root[key]; !ok {
 			return value, count > 0
 		}
 		if temp != nil {
-			value = *temp
+			value = temp.value
 			count++
 		}
 	}
