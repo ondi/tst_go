5a37a215d90a6126465c60eb23fe42e06a522e0f
ondi <thy321@gmail.com>
2024-04-11 17:01:34 +0300

    tests

--- tst3.go
+++ tst3.go
@@ -32,18 +32,20 @@ func NewTree3[Value_t any]() *Tree3_t[Value_t] {
 func (self *Tree3_t[Value_t]) Add(prefix string, value Value_t) (ok bool) {
 	var i int
 	key := key3_t{hash: FnvOffset64}
+	var temp *mapped3_t[Value_t]
 	for i, key.code = range prefix {
 		key.pos = int32(i)
 		key.hash ^= uint64(key.code)
 		key.hash *= FnvPrime64
-		if _, ok = self.root[key]; !ok {
+		if temp, ok = self.root[key]; !ok {
 			self.root[key] = nil
 		}
 	}
-	if !ok {
+	if temp == nil {
 		self.root[key] = &mapped3_t[Value_t]{value: value}
+		return true
 	}
-	return !ok
+	return false
 }
 
 func (self *Tree3_t[Value_t]) Search(in string) (value Value_t, ok bool) {
--- tst3_test.go
+++ tst3_test.go
@@ -14,10 +14,10 @@ func Test_Tst3_01(t *testing.T) {
 	temp := NewTree3[string]()
 
 	temp.Add("/", "/")
+	temp.Add("/debug/kube", "/debug/kube")
 	temp.Add("/debug", "/debug")
-	temp.Add("/debug/size", "/debug/size")
-	temp.Add("/debug/often", "/debug/often")
-	temp.Add("/debug/metrics", "/debug/metrics")
+	temp.Add("/pprof", "/pprof")
+	temp.Add("/metrics", "/metrics")
 
 	var ok bool
 	var value string
@@ -28,28 +28,27 @@ func Test_Tst3_01(t *testing.T) {
 	value, ok = temp.Search("v1/test")
 	assert.Assert(t, ok == false)
 
-	value, ok = temp.Search("/debu")
-	assert.Assert(t, ok == true)
-	assert.Assert(t, value == "/", value)
-
 	value, ok = temp.Search("/v1/test")
 	assert.Assert(t, value == "/")
+
+	value, ok = temp.Search("/debu")
+	assert.Assert(t, value == "/", value)
 
 	value, ok = temp.Search("/debug/test")
 	assert.Assert(t, value == "/debug")
 
 	value, ok = temp.Search("/debug/size")
-	assert.Assert(t, value == "/debug/size")
+	assert.Assert(t, value == "/debug")
 
-	value, ok = temp.Search("/debug/often")
-	assert.Assert(t, value == "/debug/often")
+	value, ok = temp.Search("/pprof/heap")
+	assert.Assert(t, value == "/pprof")
 
-	value, ok = temp.Search("/debug/often/very")
-	assert.Assert(t, value == "/debug/often")
+	value, ok = temp.Search("/pprof/profile")
+	assert.Assert(t, value == "/pprof")
 
-	value, ok = temp.Search("/debug/metrics")
-	assert.Assert(t, value == "/debug/metrics")
+	value, ok = temp.Search("/metrics/sql")
+	assert.Assert(t, value == "/metrics")
 
-	value, ok = temp.Search("/debug/metrics2")
-	assert.Assert(t, value == "/debug/metrics")
+	value, ok = temp.Search("/metrics/page")
+	assert.Assert(t, value == "/metrics")
 }
