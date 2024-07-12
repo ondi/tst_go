a8b2f8ecd7dabfaf6d066f330e46016479c24fcb
ondi <thy321@gmail.com>
2024-07-12 18:30:14 +0300

    minor fix

--- tst2.go
+++ tst2.go
@@ -4,6 +4,8 @@
 
 package tst
 
+import "math"
+
 type node2_t[Value_t any] struct {
 	hi_kid    int
 	eq_kid    int
@@ -22,20 +24,18 @@ type Cursor2_t[Value_t any] struct {
 	cur  int
 }
 
-const INTMAX = 1<<32 - 1
-
 func (self *Tree2_t[Value_t]) Add(in string, value Value_t) {
 	cur := 0
-	last := INTMAX
+	last := math.MaxInt
 	for _, key := range in {
 		for cur < len(self.root) && key != self.root[cur].key {
 			if key < self.root[cur].key {
-				if self.root[cur].lo_kid == INTMAX {
+				if self.root[cur].lo_kid == math.MaxInt {
 					self.root[cur].lo_kid = len(self.root)
 				}
 				cur = self.root[cur].lo_kid
 			} else {
-				if self.root[cur].hi_kid == INTMAX {
+				if self.root[cur].hi_kid == math.MaxInt {
 					self.root[cur].hi_kid = len(self.root)
 				}
 				cur = self.root[cur].hi_kid
@@ -43,15 +43,15 @@ func (self *Tree2_t[Value_t]) Add(in string, value Value_t) {
 		}
 		if cur >= len(self.root) {
 			cur = len(self.root)
-			if last != INTMAX && self.root[last].eq_kid == INTMAX {
+			if last != math.MaxInt && self.root[last].eq_kid == math.MaxInt {
 				self.root[last].eq_kid = cur
 			}
-			self.root = append(self.root, node2_t[Value_t]{key: key, eq_kid: INTMAX, hi_kid: INTMAX, lo_kid: INTMAX})
+			self.root = append(self.root, node2_t[Value_t]{key: key, eq_kid: math.MaxInt, hi_kid: math.MaxInt, lo_kid: math.MaxInt})
 		}
 		last = cur
 		cur = self.root[cur].eq_kid
 	}
-	if last != INTMAX {
+	if last != math.MaxInt {
 		self.root[last].value = value
 		self.root[last].has_value = true
 	}
@@ -72,7 +72,7 @@ func (self *Cursor2_t[Value_t]) Fetch(key rune) (value Value_t, ok bool, next bo
 			self.cur = self.root[self.cur].hi_kid
 		}
 	}
-	if self.cur == INTMAX {
+	if self.cur == math.MaxInt {
 		return
 	}
 	value = self.root[self.cur].value
